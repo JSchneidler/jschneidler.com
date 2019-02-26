4bb52d2a176fa83dd66bb0654d802d52a9608425
Jordan Schneidler <jordan.schneidler@gmail.com>
2019-02-25 18:21:59 -0600

    Commented out react-pose stuff, Gatsby will not prod build with it.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import posed, { PoseGroup } from 'react-pose';
+// import posed, { PoseGroup } from 'react-pose';
 
-import Navbar from '../components/navbar';
+// import Navbar from '../components/navbar';
 
 import './global.css';
 import './style.css';
 
+/*
 const Transition = posed.div({
   enter: { opacity: 1, x: 0, y: 0, delay: 300, beforeChildren: true },
   exit: { opacity: 0, x: ({ vertical }) => vertical ? 0 : 2000, y: ({ vertical }) => vertical ? 2000 : 0 }
@@ -16,15 +17,16 @@ const NavTransition = posed.div({
   enter: { opacity: 1, delay: 300 },
   exit: { opacity: 0, }
 });
+*/
 
 const Layout = props => (
   <>
-    <PoseGroup>
-      {props.location.pathname !== '/' && <NavTransition key="navbar"><Navbar /></NavTransition>}
-      <Transition id="transition-container" key={props.location.key} vertical={props.location.pathname === '/'}>
+    {/*<PoseGroup>*/}
+      {/*props.location.pathname !== '/' && <NavTransition key="navbar"><Navbar /></NavTransition>*/}
+      {/*<Transition id="transition-container" key={props.location.key} vertical={props.location.pathname === '/'}>*/}
         {props.children}
-      </Transition>
-    </PoseGroup>
+      {/*</Transition>*/}
+    {/*</PoseGroup>*/}
   </>
 );
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
-import posed, { PoseGroup } from 'react-pose';
+// import posed, { PoseGroup } from 'react-pose';
 
 import SEO from "../components/seo";
 import Typewriter from '../components/typewriter';
@@ -8,10 +8,12 @@ import Typewriter from '../components/typewriter';
 import background from '../images/background.jpg';
 import '../styles/index.css';
 
+/*
 const FadeInContainer = posed.div({
   enter: { opacity: 1 },
   exit: { opacity: 0 }
 });
+*/
 
 const typewriterOptions = ['Software Engineer', 'Web Developer'];
 
@@ -28,9 +30,9 @@ class IndexPage extends React.Component {
       <img id="background-image" src={background} />
       <div id="content-container">
         <div id="content">
-          <PoseGroup>{this.state.visible && <FadeInContainer key="header"><h1 id="header-text">Jordan Schneidler</h1><hr id="divider" /></FadeInContainer>}</PoseGroup>
+          {/*<PoseGroup>{this.state.visible && <FadeInContainer key="header"><h1 id="header-text">Jordan Schneidler</h1><hr id="divider" /></FadeInContainer>}</PoseGroup>*/}
           <Typewriter id="typewriter" text={typewriterOptions[Math.floor(Math.random() * typewriterOptions.length)]} />
-          <PoseGroup>
+          {/*<PoseGroup>
             {this.state.visible &&
               <FadeInContainer id="button-container" key="button-container">
                 <Link to="/work"><button className="button">My Work</button></Link>
@@ -38,7 +40,7 @@ class IndexPage extends React.Component {
                 <Link to="/contact"><button className="button">Contact Me</button></Link>
               </FadeInContainer>
             }
-          </PoseGroup>
+          </PoseGroup>*/}
         </div>
       </div>
     </>;
