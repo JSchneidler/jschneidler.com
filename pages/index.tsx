bd8c410d7327e6ad9292d18693cc2861dc0d7624
Jordan Schneidler <jordan.schneidler@gmail.com>
2021-07-22 21:19:41 -0600

    Moved index page from master

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,192 +1,97 @@
 import Head from 'next/head'
 
+import Background from '../components/Background'
+import Typewriter from '../components/Typewriter'
+
+const typewriterOptions = ['SOFTWARE ENGINEER', 'WEB DEVELOPER']
+
 export default function Home() {
+  const typewriterOption = Math.floor(Math.random() * typewriterOptions.length)
+
   return (
-    <div className="container">
+    <div className='container'>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Jordan Schneidler</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Background />
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div id='content-container'>
+          <div id='top-bar'>
+            {/*
+            <a
+              id='github-link'
+              href='https://github.com/JSchneidler/portfolio-site'
+              target='_blank'
+            >
+              <Img fixed={data.githubLogo.childImageSharp.fixed} />
+            </a>
+            */}
+          </div>
+          <div id='content'>
+            <h1 id='header-text'>JORDAN SCHNEIDLER</h1>
+            <hr id='divider' />
+            <Typewriter
+              id='typewriter'
+              text={typewriterOptions[typewriterOption]}
+            />
+          </div>
         </div>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
+        #content-container {
+          position: absolute;
+          top: 0;
           display: flex;
           flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
+          height: 100%;
           width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
+        }
+
+        #content {
           display: flex;
+          flex-direction: column;
           justify-content: center;
-          align-items: center;
+          height: 100%;
+          margin: 0 auto;
+          max-width: 85%;
+          min-width: 60%;
         }
 
-        footer img {
-          margin-left: 0.5rem;
+        #header-text {
+          color: white;
+          font-size: 2.5em;
+          text-align: center;
+          text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.5);
         }
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
+        #divider {
+          border-bottom: 2px solid white;
+          margin: 15px auto;
+          width: 200px;
         }
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
+        #typewriter {
+          color: #fdae55;
+          font-family: "Courier New", Courier, monospace;
+          font-size: 2em;
+          line-height: 30px;
+          text-shadow: 0px 0px 10px #fdae55, 0px 0px 5px #fdae55;
           text-align: center;
         }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
+        #top-bar {
+          text-align: right;
+          padding-top: 2px;
+          padding-right: 2px;
+          width: 100%;
         }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
+        #github-link {
+          background: none;
         }
       `}</style>
 
